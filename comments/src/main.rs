329eacbc61ccfd94d23101497a1cab1c7e89d516
Mahendra Prasad Dhamala <rijalmpd1234@gmail.com>
2024-02-20 22:55:00 +0545

    comments in rust

---
+++ comments/src/main.rs
@@ -0,0 +1,8 @@
+fn main() {
+    // I’m feeling lucky today
+    let lucky_number = 7;
+    //This is a value that tells whether an user is logged_in or not
+    //This is a comment
+    let is_logged_in : bool = false;
+    println!("Your lucky number is {lucky_number} and your logged in status is {is_logged_in}");
+}
